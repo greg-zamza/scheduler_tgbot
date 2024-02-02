bfe7453a1234722a91ab682710f655de85481a31
Greg Zamza <grigorev@niuitmo.ru>
2024-02-02 14:49:07 +0300

    обработка сообщений

--- BotService/main.go
+++ BotService/main.go
@@ -6,7 +6,7 @@ import (
     "io/ioutil"*/
     "strings"
     /*"strconv"*/
-    "fmt"
+    //"fmt"
     "log"
     /*"encoding/csv"
     "encoding/json"
@@ -20,12 +20,13 @@ import (
 
 /* Объявление необходимых переменных */
 var bot_token string
-
+var admin_password string
 /* Функция init() выполняется перед main() и
  * служит для определения переменных
  */
 func init() {
     bot_token = os.Getenv("BOT_TOKEN")
+    admin_password = os.Getenv("ADMIN_PASSWORD")
 }
 
 func main() {
@@ -41,16 +42,47 @@ func main() {
     defer db.Close()
 */
     // connecting to telegram API
-    //api := echotron.NewAPI(bot_token)
-    fmt.Println("ok")
+    api := echotron.NewAPI(bot_token)
+    log.Println("started")
     for update := range echotron.PollingUpdates(bot_token) {
         // помогает боту не сломаться от невалидного апдейта
         if update.Message == nil {
             log.Println("Unhandled update")
         } else {
+            var id int64 = update.ChatID()
             var words []string = strings.Fields(update.Message.Text)
-            fmt.Printf("Первое слово (команда): %s\n", words[0])
-            fmt.Println(words)
+            api.SendMessage(words[0], id, nil)
+
+            switch {
+                case words[0] == "/start":
+                    // проверка наличия пользователя в бд
+                    /* если юзер есть в бд - пишем "вы уже авторизированы как n. Вы можете выйти с помощью команды /exit" */
+                    api.SendMessage("вошь", id, nil)
+                case words[0] == "/exit":
+                    /*удаление пользователя из бд (а если его и так нет в бд - пишем: "вы не авторизованы"*/
+                    api.SendMessage("вышел", id, nil)
+                case words[0] == admin_password && len(words) == 1:
+                    /*проверка наличия пользователя в бд и если его там нет, то ставим is_admin==1*/
+                    api.SendMessage("поздравляю, ты долбоёб", id, nil)
+                /*case "":
+
+                case "":
+
+                case "":
+
+                case "":
+
+                case "":
+
+                case "":
+
+                case "":
+
+                case "":*/
+
+                default:
+                    api.SendMessage("Я не знаю такой команды", id, nil)
+            }
         }
     }
 }
