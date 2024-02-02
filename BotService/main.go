1fa13e8a3e92c3307e660f9a06bcefc6c9684e63
Greg Zamza <grigorev@niuitmo.ru>
2024-02-02 22:29:19 +0300

    реализовано всё кроме самой функции просмотра расписания

--- BotService/main.go
+++ BotService/main.go
@@ -40,7 +40,7 @@ func init() {
 }
 
 func main() {
-    // connecting to database
+    // подключение к БД
     var conn_params string = fmt.Sprintf(
         "user=%s dbname=%s sslmode=disable host=DatabaseService password=%s",
         os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_DB"), os.Getenv("POSTGRES_PASSWORD"))
@@ -51,11 +51,11 @@ func main() {
     }
     defer db.Close()
 
-    // connecting to telegram API
+    // подключение к telegram API
     api := echotron.NewAPI(botToken)
     log.Println("started")
     for update := range echotron.PollingUpdates(botToken) {
-        // помогает боту не сломаться от невалидного апдейта
+        // помогает боту не сломаться от невалидного апдейта (добавление в чат/реакция)
         if update.Message == nil {
             log.Println("Unhandled update")
         } else {
@@ -70,7 +70,6 @@ func main() {
             }
 
             for rows.Next() {
-                var unit string
                 if err := rows.Scan(&unitName, &isAdmin); err != nil {
                     log.Println(err)
                 }
@@ -92,7 +91,6 @@ func main() {
                             opts1 := echotron.MessageOptions{ReplyMarkup: markup1}
                             api.SendMessage("Добро пожаловать в расписание", id, &opts1)
                         }
-                    }
 
                 case words[0] == "/exit":
                     if (!isAdmin) && unitName == "" {
@@ -108,8 +106,12 @@ func main() {
                     if words[2] == "администратор" {
                         api.SendMessage("Отправьте пароль", id, nil)
                     } else if words[2] == "студент" {
-                        //units
+                        msg = "Посмотри список доступных групп с помощью /units и укажи свою группу с помощью /unit <группа>"
+                        api.SendMessage(msg, id, nil)
                     }
+
+                case words[0] == "/admin":
+                    api.SendMessage("Отправьте пароль", id, nil)
 
                 case words[0] == adminPassword && len(words) == 1:
                     if isAdmin {
@@ -118,16 +120,15 @@ func main() {
                          if _, err := db.Exec("UPDATE TABLE users SET is_admin = 1 WHERE id = $1", id); err != nil {
                             log.Println(err)
                          }
-                        api.SendMessage(adminText, id, nil)
                     } else {
                         if _, err := db.Exec("INSERT INTO users(id, is_admin) VALUES($1, $2)", id, 1); err != nil {
                             log.Println(err)
                         }
                     }
+                    api.SendMessage(adminText, id, nil)
 
-                /* команды, доступные администратору */
                 case words[0] == "/units":
-                    rows, err := db.Query("SELECT name FROM groups")
+                    rows, err := db.Query("SELECT name FROM units")
                     if err != nil { log.Println(err) }
                     defer rows.Close()
 
@@ -137,7 +138,7 @@ func main() {
                         if err := rows.Scan(&groupName); err != nil {
                             log.Println(err)
                         }
-                        groupNames = add(groupNames, groupName)
+                        groupNames = append(groupNames, groupName)
                     }
 
                     var sb strings.Builder
@@ -150,16 +151,17 @@ func main() {
                     finalString := sb.String()
                     api.SendMessage(finalString, id, nil)
 
+                /* команды, доступные только администратору */
                 case words[0] == "/delunit":
                     if (!isAdmin) {
                         api.SendMessage("Я не знаю такой команды", id, nil)
                     } else if len(words) != 2 {
-                        api.SendMessage("Использование: /delunit <группа>")
+                        api.SendMessage("Использование: /delunit <группа>", id, nil)
                     } else {
                         // проверка наличия группы в бд
                         var exists bool
-                        err = db.QueryRow("SELECT EXISTS (SELECT 1 FROM units WHERE name = $1)", words[1]).Scan(&exists)
-                        if err != nil { log.Println("not selected from db" }
+                        err := db.QueryRow("SELECT EXISTS (SELECT 1 FROM units WHERE name = $1)", words[1]).Scan(&exists)
+                        if err != nil { log.Println("not selected from db") }
 
                         if (!exists) {
                             msg := fmt.Sprintf("Группы %s не существует.\nВы можете посмотреть список групп с помощью /units", words[1])
@@ -172,24 +174,62 @@ func main() {
                     }
 
                 case words[0] == "/addunit":
-                    fmt.Println("q")
+                    if (!isAdmin) {
+                        api.SendMessage("Я не знаю такой команды", id, nil)
+                    } else if len(words) != 2 {
+                        api.SendMessage("Использование: /addunit <группа>", id, nil)
+                    } else {
+                        // проверка наличия группы в бд
+                        var exists bool
+                        err := db.QueryRow("SELECT EXISTS (SELECT 1 FROM units WHERE name = $1)", words[1]).Scan(&exists)
+                        if err != nil { log.Println("not selected from db") }
 
-                case words[0] == "s":
+                        if (!exists) {
+                            msg := fmt.Sprintf("Группы %s не существует.\nВы можете посмотреть список групп с помощью /units", words[1])
+                            api.SendMessage(msg, id, nil)
+                        } else {
+                            _, err := db.Exec("INSERT INTO units(name) VALUES($1)", words[1])
+                            if err != nil { log.Println("not inserted") }
+                            api.SendMessage(fmt.Sprintf("Группа %s добавлена.", words[1]), id, nil)
+                        }
+                    }
+
+                case words[0] == "/s":
                     //расписание
-                    fmt.Println("q")
+                    if isAdmin {
+                        if len(words) != 3 {
+                            api.SendMessage("Использование: /s <номер группы> <ДД.ММ.ГГГГ>", id, nil)
+                        } else {
+                            /*TODO тут мы делаем запрос к базе данных и формируем расписание на день*/
+                        }
+                    } else if unitName != "" {
+                        if len(words) !=2 {
+                            api.SendMessage("Использование: /s <ДД.ММ.ГГГГ>", id, nil)
+                        } else {
+                            /*TODO тут мы делаем запрос к базе данных и формируем расписание на день*/
+                        }
+                    } else {
+                        msg ="Укажи свою группу с помощью /unit <группа> или войди в аккаунт администратора c помощью /admin.\nСписок групп можно посмотреть с помощью /units"
+                        api.SendMessage(msg, id, nil)
+                    }
 
                 case words[0] == "/add":
-                    //добавить
-                    fmt.Println("q")
+                    /*TODO тут будет то же самое, что и в /del, просто вместо DELETE будет INSERT INTO */
 
                 case words[0] == "/del":
-                    //удалить
-                    fmt.Println("q")
+                    if (!isAdmin) {
+                        api.SendMessage("Я не знаю такой команды", id, nil)
+                    } else if len(words) != 4 {
+                        api.SendMessage("Использование: /del <группа> <ДД.ММ.ГГГГ> <номер пары>", id, nil)
+                    } else {
+                        /*TODO тут стоит проверить, конвертируется ли строка в дату и существует ли такая пара
+                        * если нет - отправляем пользователю usage. Если всё ок - удаляем и пишем об успехе*/
+                    }
 
                 /* команды, доступные студентам */
                 case words[0] == "/unit":
-                    //объявить свой класс
-                    fmt.Println("Q")
+                    /*TODO проверка количества аргументов, потом проверка, существует ли такая группа
+                     * если существует, то присваиваем полю users.unit переданное значение words[1]*/
 
                 /* Команды "/s" и "/units" также доступны студентам, но
                  * обрабатываются в тех же кейсах, что и у администраторов
